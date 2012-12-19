12e43c403543362d69f8c4bb2867eddb0e52fb4c
Damian Suarez <rdsuarez@gmail.com>
2012-12-18 23:28:25 -0300

    test: add `array of items` tests

--- test/index.js
+++ test/index.js
@@ -2,12 +2,24 @@ var remove = require('../');
 
 describe('remove(string, array)', function(){
   it('should remove the first matched item', function(){
-    var arr = ['a', 'b', 'c', 'b', 'd', 'b'];
-    remove('b', arr).should.be.eql(['a', 'c', 'b', 'd', 'b']);
+    var arr = [1, 2, 3, 1, 2, 3, 1, 2, 3];
+    remove(2, arr, false).should.be.eql([1, 3, 1, 2, 3, 1, 2, 3]);
   });
 
   it('should remove all matched items', function(){
-    var arr = ['a', 'b', 'c', 'b', 'd', 'b'];
-    remove('b', arr, true).should.be.eql(['a', 'c', 'd']);
+    var arr = [1, 2, 3, 1, 2, 3, 1, 2, 3];
+    remove(2, arr).should.be.eql([1, 3, 1, 3, 1, 3]);
+  });
+});
+
+describe('remove(array, array)', function(){
+  it('should remove the first matched item', function(){
+    var arr = [1, 2, 3, 1, 2, 3, 1, 2, 3];
+    remove([2, 1], arr, false).should.be.eql([3, 1, 2, 3, 1, 2, 3]);
+  });
+
+  it('should remove all matched items', function(){
+    var arr = [1, 2, 3, 1, 2, 3, 1, 2, 3];
+    remove([2, 1], arr).should.be.eql([3, 3, 3]);
   });
 });
