d67f92b448220720fa0e5e0f3f757a22097f888f
Damian Suarez <rdsuarez@gmail.com>
2012-12-18 23:27:56 -0300

    core: implement array of items

--- index.js
+++ index.js
@@ -15,16 +15,27 @@ try {
  * Remove the given element from the give array
  *
  * @param item
- * @param {Array} arr array of items to remove
- * @param {Boolen} multiple
+ * @param {Array} arr array of items to remove the item/s
+ * @param {Boolen} multiple (optional - default true)
  * @return {Array}
  * @api public
  */
 
 module.exports = function (item, arr, multiple) {
-  var ind = indexOf(arr, item);
-  if (ind < 0) return arr;
-  if (multiple) arr = uniq(arr, item);
-  arr.splice(ind, 1);
+  if (!(item instanceof Array)) {
+    return removeItem(item, arr, multiple);
+  }
+
+  for (var i = 0; i < item.length; i++) {
+    arr = removeItem(item[i], arr, multiple);
+  }
   return arr;
 };
+
+function removeItem(item, arr, multiple){
+  var ind = indexOf(arr, item);
+  if (ind < 0) return arr;
+  if (false !== multiple) arr = uniq(arr, item);
+  arr.splice(ind, 1);
+  return arr;
+}
